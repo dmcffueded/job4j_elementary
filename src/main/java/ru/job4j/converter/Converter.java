455bc84d06e5b490003d4197dc84037708097297
Andrei Borisov <charonas@yandex.ru>
2024-08-18 20:06:28 +0300

    добавил пробел

--- src/main/java/ru/job4j/converter/Converter.java
+++ src/main/java/ru/job4j/converter/Converter.java
@@ -9,6 +9,6 @@ public class Converter {
 
     public static double rubleToDollar(double value) {
         // Assuming the conversion rate is 1 Dollar = 75 Rubles
-        return value /75;
+        return value / 75;
     }
 }
