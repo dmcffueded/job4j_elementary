e6692623f5f6328a5c69a73b61ceb08b41471ab4
Andrei Borisov <charonas@yandex.ru>
2024-08-20 20:36:34 +0300

    убрал else

--- src/main/java/ru/job4j/loop/CheckPrimeNumber.java
+++ src/main/java/ru/job4j/loop/CheckPrimeNumber.java
@@ -3,18 +3,16 @@ package ru.job4j.loop;
 public class CheckPrimeNumber {
 
     public static boolean check(int number) {
-
-        boolean result = true;
-        if (number == 1) {
-            result = false;
+        if (number <= 1) {
+            return false;
         }
+
         for (int i = 2; i <= number - 1; i++) {
             if (number % i == 0) {
-                result = false;
-                break;
+                return false;
             }
         }
-        return result;
+        return true;
     }
 
 }
