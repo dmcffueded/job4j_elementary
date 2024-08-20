1cb32acb4bad9e30696832db2cf427b164cfe1a2
Andrei Borisov <charonas@yandex.ru>
2024-08-20 15:34:00 +0300

    множественный return

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -2,14 +2,14 @@ package ru.job4j.loop;
 
 public class Counter {
     public static int sum(int start, int finish) {
-        int sum = 0;
         if (start <= finish) {
+            int sum = 0;
             for (int i = start; i <= finish; i++) {
                 sum = sum + i;
             }
             return sum;
         }
-        return sum;
+        return 0;
     }
 
     public static int sumByEven(int start, int finish) {
