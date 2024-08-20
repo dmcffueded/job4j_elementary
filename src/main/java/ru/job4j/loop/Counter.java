2407cf42a8456ac2348efb6d36c2dbee0df366d4
Andrei Borisov <charonas@yandex.ru>
2024-08-20 15:40:30 +0300

    еще коммит

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -13,16 +13,16 @@ public class Counter {
     }
 
     public static int sumByEven(int start, int finish) {
-        int sum = 0;
         if (start <= finish) {
+            int sum = 0;
             for (int i = start; i <= finish; i++) {
                 if (i % 2 == 0) {
-                sum = sum + i;
+                    sum = sum + i;
                 }
             }
             return sum;
         }
-        return sum;
+        return 0;
     }
 
     public static void main(String[] args) {
