23554bb8c867ef53c1f9880389da93202a5ed102
Andrei Borisov <charonas@yandex.ru>
2024-08-21 05:34:34 +0300

    правка sumByEven

--- src/main/java/ru/job4j/loop/Counter.java
+++ src/main/java/ru/job4j/loop/Counter.java
@@ -13,16 +13,13 @@ public class Counter {
     }
 
     public static int sumByEven(int start, int finish) {
-        if (start <= finish) {
-            int sum = 0;
+        int sum = 0;
             for (int i = start; i <= finish; i++) {
                 if (i % 2 == 0) {
                     sum = sum + i;
                 }
             }
             return sum;
-        }
-        return 0;
     }
 
     public static void main(String[] args) {
