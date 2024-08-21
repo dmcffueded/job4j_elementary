07e773e4e9b589a62b87ed17a00f0d731b128201
Andrei Borisov <charonas@yandex.ru>
2024-08-21 16:44:40 +0300

    заполнение массива [#505222]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class ArrayDefinition {
+
     public static void main(String[] args) {
 
         short[] ages = new short[10];
@@ -10,6 +11,16 @@ public class ArrayDefinition {
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
+
+        String[] names = new String[4];
+        names[0] = "Andrei";
+        names[1] = "Oleg";
+        names[2] = "Regina";
+        names[3] = "Dragos";
+
+        for (int i = 0; i <= 3; i++) {
+            System.out.println("В " + i + " ячейке массива содержится " + names[i]);
+        }
 
     }
 }
