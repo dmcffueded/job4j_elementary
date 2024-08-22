4dca1be9caabc76e8cf0c1a94de4967f326d634d
Andrei Borisov <charonas@yandex.ru>
2024-08-22 15:55:49 +0300

    cортировка выборкой [#505240]

---
+++ src/main/java/ru/job4j/array/SortSelected.java
@@ -0,0 +1,16 @@
+package ru.job4j.array;
+
+import static ru.job4j.array.SwitchArray.swap;
+
+public class SortSelected {
+
+    public static int[] sort(int[] data) {
+        for (int i = 0; i < data.length; i++) {
+            int min = MinDiapason.findMin(data, i, data.length - 1);
+            int index = FindLoop.indexInRange(data, min, i, data.length - 1);
+            swap(data, index, i);
+        }
+        return data;
+    }
+
+}
