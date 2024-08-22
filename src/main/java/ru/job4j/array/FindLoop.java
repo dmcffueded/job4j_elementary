d7c50b13b2bafc21ef916c191dec0f8b1376982b
Andrei Borisov <charonas@yandex.ru>
2024-08-22 15:13:49 +0300

    поиск индекса в диапазоне [#505237]

--- src/main/java/ru/job4j/array/FindLoop.java
+++ src/main/java/ru/job4j/array/FindLoop.java
@@ -1,6 +1,7 @@
 package ru.job4j.array;
 
 public class FindLoop {
+
     public static int indexOf(int[] data, int element) {
         int result = -1;
         for (int index = 0; index < data.length; index++) {
@@ -11,4 +12,16 @@ public class FindLoop {
         }
         return result;
     }
+
+    public static int indexInRange(int[] data, int element, int start, int finish) {
+        int result = -1;
+        for (int index = start; index <= finish; index++) {
+            if (data[index] == element) {
+                result = index;
+                break;
+            }
+        }
+        return result;
+    }
+
 }
