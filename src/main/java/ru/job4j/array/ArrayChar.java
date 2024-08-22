9f9fa98a6e9ca49b15c5855ec1546c87271fd7fb
Andrei Borisov <charonas@yandex.ru>
2024-08-22 16:32:59 +0300

    правка [#505233 #510171]

--- src/main/java/ru/job4j/array/ArrayChar.java
+++ src/main/java/ru/job4j/array/ArrayChar.java
@@ -5,7 +5,7 @@ public class ArrayChar {
     public static boolean startsWith(char[] word, char[] prefix) {
         boolean result = true;
         for (int i = 0; i < prefix.length; i++) {
-            if (!(prefix[i] == word[i])) {
+            if (prefix[i] != word[i]) {
                 result = false;
                 break;
             }
