564b4537ef887fd60872da86d4e65ff14528ef78
Andrei Borisov <charonas@yandex.ru>
2024-08-23 13:37:37 +0300

    убрал SortSelected.sort(array)

--- src/main/java/ru/job4j/array/TwoNumberSum.java
+++ src/main/java/ru/job4j/array/TwoNumberSum.java
@@ -2,7 +2,6 @@ package ru.job4j.array;
 
 public class TwoNumberSum {
     public static int[] getIndexes(int[] array, int target) {
-        SortSelected.sort(array);
         int i = 0;
         int j = array.length - 1;
         while (i < j) {
