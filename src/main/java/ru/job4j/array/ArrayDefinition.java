cd9c01840d28d2bdc16263c6a7bededb7bcc5e0d
Andrei Borisov <charonas@yandex.ru>
2024-08-26 12:00:34 +0300

    чтение кода и базовые ошибки [#40205]

--- src/main/java/ru/job4j/array/ArrayDefinition.java
+++ src/main/java/ru/job4j/array/ArrayDefinition.java
@@ -3,24 +3,19 @@ package ru.job4j.array;
 public class ArrayDefinition {
 
     public static void main(String[] args) {
-
         short[] ages = new short[10];
         String[] surnames = new String[100500];
         float[] prices = new float[40];
-
         System.out.println("Размер массива равен: " + ages.length);
         System.out.println("Размер массива равен: " + surnames.length);
         System.out.println("Размер массива равен: " + prices.length);
-
         String[] names = new String[4];
         names[0] = "Andrei";
         names[1] = "Oleg";
         names[2] = "Regina";
         names[3] = "Dragos";
-
         for (int i = 0; i <= 3; i++) {
             System.out.println("В " + i + " ячейке массива содержится " + names[i]);
         }
-
     }
 }
